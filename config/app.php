a9be959d60b188f33270896ab8ca6666c6b6bc69
MIKEY\hsuch <hsuchengshuo@126.com>
2021-01-19 19:13:27 +0800

    no message

--- config/app.php
+++ config/app.php
@@ -166,7 +166,7 @@ return [
         /*
          * Package Service Providers...
          */
-
+        SimpleSoftwareIO\QrCode\QrCodeServiceProvider::class,
         /*
          * Application Service Providers...
          */
@@ -229,6 +229,7 @@ return [
         'View' => Illuminate\Support\Facades\View::class,
         'Form'=> Collective\Html\FormFacade::class,
         'Html'=> Collective\Html\HtmlFacade::class,
+        'QrCode' => SimpleSoftwareIO\QrCode\Facades\QrCode::class
     ],
 
 ];
